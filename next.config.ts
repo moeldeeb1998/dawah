1f847d53b5c098b9c948b7d44a0cef4bfd681747
Mohamed Eldeeb <moeldeeb98@gmail.com>
2025-10-08 11:40:59 +0300

    disables built-in optimization for static public files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true, // disables built-in optimization
+  },
+  output: "standalone", // ensures production server runs cleanly
 };
 
 export default nextConfig;
